23bc4bb088149a5d2b6b4dcce31d534bb82406d7
521852 <debabrata.bardhan@seagate.com>
2018-08-28 00:47:01 +0530

    correcting wrongly implemented bit wise and

--- src/nvme_cmds.c
+++ src/nvme_cmds.c
@@ -624,7 +624,7 @@ int nvme_Get_Log_Page(tDevice *device, nvmeGetLogPageCmdOpts * getLogPageCmdOpts
     getLogPage.cmd.adminCmd.cdw10 = dWord10;
     getLogPage.cmd.adminCmd.cdw11 = numDwords >> 16;
 
-    getLogPage.cmd.adminCmd.cdw12 = (uint32_t)(getLogPageCmdOpts->offset && 0xFFFFFFFF);
+    getLogPage.cmd.adminCmd.cdw12 = (uint32_t)(getLogPageCmdOpts->offset & 0xFFFFFFFF);
     getLogPage.cmd.adminCmd.cdw13 = (uint32_t)(getLogPageCmdOpts->offset >> 32);
 
 	getLogPage.ptrData = (uint8_t*)getLogPageCmdOpts->addr;
