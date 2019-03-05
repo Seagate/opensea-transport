53c7b18219c30221a40ee86326cf5f8041c16dbc
Muhammad Ahmad <muhammad.ahmad@seagate.com>
2019-03-04 23:50:08 -0600

    Automating the updating of versions

---
+++ update-version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+: '
+//
+// Do NOT modify or remove this copyright and license
+//
+// Copyright (c) 2012 - 2019 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
+//
+// This software is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// ******************************************************************************************
+//
+// \file update-version.h
+// \brief Update the version number & check in code.
+//
+'
+VERSION_FILE=include/version.h
+BASE=`awk '$2 == "OPENSEA_TRANSPORT_PATCH_VERSION" {print $1 "  " $2}' "$VERSION_FILE"`
+CURRENT_VERSION=`awk '$2 == "OPENSEA_TRANSPORT_PATCH_VERSION" {print $3}' "$VERSION_FILE"`
+NEXT_VER=$((CURRENT_VERSION + 1))
+echo $BASE $CURRENT_VERSION
+echo $BASE $NEXT_VER
+sed -i "/._PATCH_VERSION\s*/s/$CURRENT_VERSION/$NEXT_VER/" "$VERSION_FILE"
+
+MAKE_FILE=Make/gcc/Makefile
+sed -i "/PATCH=*/s/$CURRENT_VERSION/$NEXT_VER/" "$MAKE_FILE"
