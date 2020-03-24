7f9e94e1db0334fde14aeb9fa1b9a5a2cb87482e
Steve Peters <steven.peters@seagate.com>
2020-03-24 11:51:59 -0600

    add firecuda 120 support

--- src/common_public.c
+++ src/common_public.c
@@ -1294,6 +1294,9 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
 			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
 				&& (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "MC") == 7))		// BC125
             ||
+			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "GM") == 7))		// FireCuda 120
+			||
             ((strstr(device->drive_info.bridge_info.childDriveMN, "YA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "CM") == 7))
             ||
@@ -1319,6 +1322,9 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
 			((strstr(device->drive_info.product_identification, "ZA") != NULL)
 				&& (find_last_occurrence_in_string(device->drive_info.product_identification, "MC") == 7))					// BC125
             ||
+			((strstr(device->drive_info.product_identification, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.product_identification, "GM") == 7))					// BC125
+			||
             ((strstr(device->drive_info.product_identification, "YA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.product_identification, "CM") == 7))
             ||
