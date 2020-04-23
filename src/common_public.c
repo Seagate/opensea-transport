f928878df8cb058be21693578d9a89019890dac3
Steven Peters <steven.peters@seagate.com>
2020-04-23 14:39:09 -0600

    add SATA firecuda

--- src/common_public.c
+++ src/common_public.c
@@ -1292,6 +1292,9 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
             ((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "CM") == 7))
             ||
+			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "GM") == 7))
+			||
             ((strstr(device->drive_info.bridge_info.childDriveMN, "YA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "CM") == 7))
             ||
@@ -1311,6 +1314,9 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
             ((strstr(device->drive_info.product_identification, "ZA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.product_identification, "CM") == 7))
             ||
+			((strstr(device->drive_info.product_identification, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.product_identification, "GM") == 7))
+			||
             ((strstr(device->drive_info.product_identification, "YA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.product_identification, "CM") == 7))
             ||
