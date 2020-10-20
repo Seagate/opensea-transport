64e24bc60f11d61ded90bf32473ae0c2e831953d
Steven Peters <steven.peters@seagate.com>
2020-10-20 11:31:19 -0600

    Add BarraCuda Q1

--- src/common_public.c
+++ src/common_public.c
@@ -1296,6 +1296,9 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
                 && (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "CM") == 7))
             ||
 			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "CV") == 7))
+			||
+			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
 				&& (find_last_occurrence_in_string(device->drive_info.bridge_info.childDriveMN, "MC") == 7))
 			|| 
 			((strstr(device->drive_info.bridge_info.childDriveMN, "ZA") != NULL)
@@ -1335,6 +1338,10 @@ bool is_Seagate_Model_Number_Vendor_F(tDevice *device, bool USBchildDrive)
             ((strstr(device->drive_info.product_identification, "ZA") != NULL)
                 && (find_last_occurrence_in_string(device->drive_info.product_identification, "CM") == 7))
             ||
+			((strstr(device->drive_info.product_identification, "ZA") != NULL)
+				&& (find_last_occurrence_in_string(device->drive_info.product_identification, "CV") == 7))
+			||
+
 			((strstr(device->drive_info.product_identification, "ZA") != NULL)
 				&& (find_last_occurrence_in_string(device->drive_info.product_identification, "MC") == 7))
 			|| 
