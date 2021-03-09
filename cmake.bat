1c71cdb9aaa1826cd1b20d2b23f5181d92ee0150
pranali-tirkhunde <pranali.tirkhunde@seagate.com>
2021-03-09 11:58:41 +0530

    quick: Updating cmake.bat to change directory to the corresponding platform after creation.

--- cmake.bat
+++ cmake.bat
@@ -14,19 +14,43 @@ if "%1" == "-arm" goto arm
 if "%1" == "-arm64" goto arm64
 
 :64bit
-if not exist "%VS-Version%/x64" (mkdir "%VS-Version%/x64")
+if not exist "%VS-Version%/x64" (goto handle64bit)
 goto out
 
 :32bit
-if not exist "%VS-Version%/x86" (mkdir "%VS-Version%/x86")
+if not exist "%VS-Version%/x86" (goto handle32bit)
 goto out
 
 :arm
-if not exist "%VS-Version%/arm" (mkdir "%VS-Version%/arm")
+if not exist "%VS-Version%/arm" (goto handlearm)
 goto out
 
 :arm64
-if not exist "%VS-Version%/arm64" (mkdir "%VS-Version%/arm64")
+if not exist "%VS-Version%/arm64" (goto handlearm64)
+goto out
+
+:handle64bit
+mkdir "%VS-Version%\x64"
+set pathname= %__CD__%%VS-Version%\x64
+cd %pathname%
+goto out
+
+:handle32bit
+mkdir "%VS-Version%\x86"
+set pathname= %__CD__%%VS-Version%\x86
+cd %pathname%
+goto out
+
+:handlearm
+mkdir "%VS-Version%\arm"
+set pathname= %__CD__%%VS-Version%\arm
+cd %pathname%
+goto out
+
+:handlearm64
+mkdir "%VS-Version%\arm64"
+set pathname= %__CD__%%VS-Version%\arm64
+cd %pathname%
 goto out
 
 :out
