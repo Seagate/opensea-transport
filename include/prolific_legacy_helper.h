316175db363952e271c1a6eb3f94026ff5e0dc17
Tyler Erickson <tyler.erickson@seagate.com>
2025-04-22 10:06:36 -0600

    doc: Updating copyright year in various files/comments to 2025
    Updating 2024 to 2025 for correct copyright.
    
    Signed-off-by: Tyler Erickson <tyler.erickson@seagate.com>

--- include/prolific_legacy_helper.h
+++ include/prolific_legacy_helper.h
@@ -8,7 +8,7 @@
 //! \copyright
 //! Do NOT modify or remove this copyright and license
 //!
-//! Copyright (c) 2012-2024 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
+//! Copyright (c) 2012-2025 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
 //!
 //! This software is subject to the terms of the Mozilla Public License, v. 2.0.
 //! If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
